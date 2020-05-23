ec3adf7f9825b0575ec086b5bc422610e8bdf401
ab7289 <ab7289@nyu.edu>
2020-05-23 15:57:19 -0400

    moved around settings and requirements

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipesoup.settings")
+# This allows easy placement of apps within the interior
+# recipesoup directory.
+app_path = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+)
+sys.path.append(os.path.join(app_path, "recipesoup"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 application = get_wsgi_application()
